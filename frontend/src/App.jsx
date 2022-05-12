0d2bb3a7b5ceb9956695f895e9e2e85c1c3e86cb
ClemDSC <clemence.p14@gmail.com>
2022-05-12 15:50:45 +0200

    creation page home pour l'accueil + creation component dailychallenge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
 import "../src/styles/App.css";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Form_Inscription />} />
-        <Route path="/accueil" element={<Accueil />} />
-        <Route path="/activites" element={<Activites_Globales />} />
+        {/* <Route path="/" element={<Form_Inscription />} /> */}
+        <Route path="/accueil" element={<Home />} />
+        {/* <Route path="/activites" element={<Activites_Globales />} />
         <Route path="/evenements" element={<Evenements_Globales />} />
         <Route path="/dashboard" element={<Dashboard />} /> */}
       </Routes>
