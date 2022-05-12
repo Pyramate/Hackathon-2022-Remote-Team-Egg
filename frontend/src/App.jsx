7cf4cfe874e3dff3ce4375a67c25cb4ac206081d
ClemDSC <clemence.p14@gmail.com>
2022-05-12 15:31:21 +0200

    installation de react-router-dom + creation des routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
-import '../src/styles/App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "../src/styles/App.css";
 
 function App() {
   return (
-    <div></div>
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Form_Inscription />} />
+        <Route path="/accueil" element={<Accueil />} />
+        <Route path="/activites" element={<Activites_Globales />} />
+        <Route path="/evenements" element={<Evenements_Globales />} />
+        <Route path="/dashboard" element={<Dashboard />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
