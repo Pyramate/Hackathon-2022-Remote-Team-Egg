80af61ce9e0c84b7c03da3f516ffded28a2c8403
Julie Combeau <combeau.julie@gmail.com>
2022-05-12 23:52:37 +0200

    remove component to Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import DailyChallenge from "../components/DailyChallenge";
-import CardActivity from "../components/CardActivity";
 
 export default function Home() {
   return (
     <div>
       <DailyChallenge />
-      <CardActivity />
     </div>
   );
 }
