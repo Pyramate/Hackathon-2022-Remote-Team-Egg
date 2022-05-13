90cfa70881758fd0091090e21978c961f3f4f77c
Sylvain LEGUAY <leguay.sylvain40@gmail.com>
2022-05-13 12:18:36 +0200

    changed activities

--- frontend/src/pages/ActivitesGlobales.jsx
+++ frontend/src/pages/ActivitesGlobales.jsx
@@ -1,7 +1,16 @@
-import logoactivite from '../assets/bglogo.png';
-import { Box, Container, Text, Flex } from '@chakra-ui/react';
-import CarouselGlobalActivitiesPages from '../components/CarouselGlobalActivitiesPages';
-import Header from '../components/Header';
+import logoactivite from "../assets/image_activites.jpeg";
+import {
+  Box,
+  Container,
+  Text,
+  Flex,
+  VStack,
+  Stack,
+  Button,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import CarouselGlobalActivitiesPages from "../components/CarouselGlobalActivitiesPages";
+import Header from "../components/Header";
 
 import background from "../assets/background.png";
 
@@ -10,8 +19,7 @@ export default function ActivitesGlobales() {
     <>
       <Header />
       <Box bgImage={background} bgRepeat="no-repeat" bgSize="cover">
-        <Header />
-        <Container
+        <Flex
           bgImage={logoactivite}
           backgroundSize={"cover"}
           backgroundPosition={"center center"}
@@ -20,74 +28,47 @@ export default function ActivitesGlobales() {
             w={"full"}
             justify={"center"}
             px={useBreakpointValue({ base: 4, md: 8 })}
-            bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
+            bgGradient={"linear(to-r, blackAlpha.600, blackAlpha.400)"}
           >
-            <Stack maxW={"2xl"} align={"flex-start"} spacing={35}>
+            <Stack my="7rem" maxW={"2xl"} align={"flex-start"} spacing={25}>
               <Text
                 color={"white"}
                 fontWeight={700}
                 lineHeight={1.2}
                 fontSize={useBreakpointValue({ base: "3xl", md: "4xl" })}
-                mt="15rem"
               >
-                Lorem ipsum dolor sit amet consectetur adipiscing elit sed do
-                eiusmod tempor
+                Découvre les activités à faire avec ta tribu.
               </Text>
-              <Stack direction={"row"}>
-                <Button
-                  bg={"green.600"}
-                  rounded={"full"}
-                  color={"white"}
-                  _hover={{ bg: "green.500" }}
-                >
-                  Voir les activités
-                </Button>
-              </Stack>
             </Stack>
           </VStack>
         </Flex>
+        <Flex w="90vw" flexDirection="column">
+          <Flex mt="3rem" flexDirection="column">
+            <Text color={"green.500"} fontSize="4xl" fontWeight="600" ml="5rem">
+              Recettes
+            </Text>
+            <CarouselGlobalActivitiesPages genre={"food"} />
+          </Flex>
+          <Flex mt="3rem" flexDirection="column">
+            <Text color={"green.500"} fontSize="4xl" fontWeight="600" ml="5rem">
+              Cosmétiques{" "}
+            </Text>
 
-        <Flex mt="3rem" flexDirection="column">
-          <Text
-            color={"green.500"}
-            fontSize={{ base: "sm", sm: "2xl" }}
-            ml="5rem"
-          >
-            Recettes{" "}
-          </Text>
-          <CarouselGlobalActivitiesPages genre={"food"} />
-        </Flex>
-        <Flex mt="3rem" flexDirection="column">
-          <Text
-            color={"green.500"}
-            fontSize={{ base: "sm", sm: "2xl" }}
-            ml="5rem"
-          >
-            Cosmétiques{" "}
-          </Text>
+            <CarouselGlobalActivitiesPages genre={"Hygiène"} />
+          </Flex>
+          <Flex mt="3rem" flexDirection="column">
+            <Text color={"green.500"} fontSize="4xl" fontWeight="600" ml="5rem">
+              Produits Entretiens{" "}
+            </Text>
 
-          <CarouselGlobalActivitiesPages genre={"Hygiène"} />
-        </Flex>
-        <Flex mt="3rem" flexDirection="column">
-          <Text
-            color={"green.500"}
-            fontSize={{ base: "sm", sm: "2xl" }}
-            ml="5rem"
-          >
-            Produits Entretiens{" "}
-          </Text>
-
-          <CarouselGlobalActivitiesPages genre={"Entretien"} />
-        </Flex>
-        <Flex mt="3rem" flexDirection="column" id="garden">
-          <Text
-            color={"green.500"}
-            fontSize={{ base: "sm", sm: "2xl" }}
-            ml="5rem"
-          >
-            Jardin{" "}
-          </Text>
-          <CarouselGlobalActivitiesPages genre={"Jardin"} />
+            <CarouselGlobalActivitiesPages genre={"Entretien"} />
+          </Flex>
+          <Flex mt="3rem" flexDirection="column" id="garden">
+            <Text color={"green.500"} fontSize="4xl" fontWeight="600" ml="5rem">
+              Jardin{" "}
+            </Text>
+            <CarouselGlobalActivitiesPages genre={"Jardin"} />
+          </Flex>
         </Flex>
       </Box>
     </>
